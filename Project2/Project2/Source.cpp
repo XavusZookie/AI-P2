afe6b5d85d4141a9ec2c27b3c7020a1adc5f4cc1
XavusZookie <xavuszookie@gmail.com>
2022-11-03 00:05:48 -0400

    movement started
    added in the AIs ability to track movement and update based of where the movements were

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -11,7 +11,7 @@ public:
 	//obstacle?
 	bool west, north, east, south;
 	double topprob;
-
+	double temp;
 
 	squares(bool twest, bool tnorth, bool teast, bool tsouth)
 	{
@@ -20,13 +20,34 @@ public:
 		east = teast;
 		south = tsouth;
 		topprob = 1;
+		temp = 0;
 	}
 
 	squares(double obstacle)
 	{
 		topprob = obstacle;
+		west = true;
+		north = true;
+		east = true;
+		south = true;
+		topprob = 0;
+		temp = 0;
 	}
 
+};
+
+class sense
+{
+public:
+	//obstacle
+	bool west, north, east, south;
+	sense(bool twest, bool tnorth, bool teast, bool tsouth)
+	{
+		west = twest;
+		north = tnorth;
+		east = teast;
+		south = tsouth;
+	}
 };
 
 class map
@@ -79,9 +100,366 @@ public:
 		}
 	}
 
+	void firstsenseupdate(sense first)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				if (arr[i][j].topprob != 0)
+				{
+					if (first.west == true)
+					{
+						if (arr[i][j].west == true)
+						{
+							arr[i][j].topprob *= .8;
+						}
+						else
+						{
+							arr[i][j].topprob *= .15;
+						}
+					}
+					else
+					{
+						if (arr[i][j].west == true)
+						{
+							arr[i][j].topprob *= .2;
+						}
+						else
+						{
+							arr[i][j].topprob *= .85;
+						}
+					}
+
+					if (first.north == true)
+					{
+						if (arr[i][j].north == true)
+						{
+							arr[i][j].topprob *= .8;
+						}
+						else
+						{
+							arr[i][j].topprob *= .15;
+						}
+					}
+					else
+					{
+						if (arr[i][j].north == true)
+						{
+							arr[i][j].topprob *= .2;
+						}
+						else
+						{
+							arr[i][j].topprob *= .85;
+						}
+					}
+
+					if (first.east == true)
+					{
+						if (arr[i][j].east == true)
+						{
+							arr[i][j].topprob *= .8;
+						}
+						else
+						{
+							arr[i][j].topprob *= .15;
+						}
+					}
+					else
+					{
+						if (arr[i][j].east == true)
+						{
+							arr[i][j].topprob *= .2;
+						}
+						else
+						{
+							arr[i][j].topprob *= .85;
+						}
+					}
+
+					if (first.south == true)
+					{
+						if (arr[i][j].south == true)
+						{
+							arr[i][j].topprob *= .8;
+						}
+						else
+						{
+							arr[i][j].topprob *= .15;
+						}
+					}
+					else
+					{
+						if (arr[i][j].south == true)
+						{
+							arr[i][j].topprob *= .2;
+						}
+						else
+						{
+							arr[i][j].topprob *= .85;
+						}
+					}
+
+				}
+			}
+		}
+	}
+
+	void finalsenseupdate()
+	{
+	
+
+
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				if (arr[i][j].topprob != 0)
+				{
+					arr[i][j].topprob = arr[i][j].topprob / (currentbottom);
+				}
+			}
+		}
+	}
+
+	void firstmovementupdate(int dir)
+	{
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
 
 
 
+				if (dir == 1)
+				{
+
+					if (i > 0)
+					{
+						if (arr[i - 1][j].topprob != 0)
+						{
+							arr[i][j].temp += arr[i - 1][j].topprob * .1;  // left
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+					}
+					else
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+					if (i < 6)
+					{
+
+						if (arr[i + 1][j].topprob != 0)
+						{
+							arr[i][j].temp += arr[i + 1][j].topprob * .8;  // right
+						}
+
+					}
+
+					if (j > 0)
+					{
+						if (arr[i][j - 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j - 1].topprob * .1;  // top
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+
+					}
+					else
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+					if (j < 7)
+					{
+						if (arr[i][j + 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j + 1].topprob * .1;  // bottom
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+
+					}
+					else
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+
+				}
+					// 1 west 2 north 3 east 4 south
+
+				//done
+				if (dir == 1)
+					{
+
+						if (i > 0)
+						{
+							if (arr[i - 1][j].topprob != 0)
+							{
+								  // left
+							}
+							else
+							{
+								arr[i][j].temp += arr[i][j].topprob * .1;
+							}
+
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+						if (i < 6)
+						{
+
+							if (arr[i + 1][j].topprob != 0)
+							{
+								arr[i][j].temp += arr[i + 1][j].topprob * .8;  // right
+							}
+						
+						}
+
+						if (j > 0)
+						{
+							if (arr[i ][j-1].topprob != 0)
+							{
+								arr[i][j].temp += arr[i][j - 1].topprob * .1;  // top
+							}
+							else
+							{
+								arr[i][j].temp += arr[i][j].topprob * .1;
+							}
+
+							
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+						if (j < 7)
+						{
+							if (arr[i][j + 1].topprob != 0)
+							{
+								arr[i][j].temp += arr[i][j + 1].topprob * .1;  // bottom
+							}
+							else
+							{
+								arr[i][j].temp += arr[i][j].topprob * .1;
+							}
+
+
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+
+					}
+
+				if (dir == 2)
+				{
+
+					if (i > 0)
+					{
+						if (arr[i - 1][j].topprob != 0)
+						{
+							arr[i][j].temp += arr[i - 1][j].topprob * .1;  // left
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+					}
+					else
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+					if (i < 6)
+					{
+
+						if (arr[i + 1][j].topprob != 0)
+						{
+							arr[i][j].temp += arr[i + 1][j].topprob * .8;  // right
+						}
+
+					}
+
+					if (j > 0)
+					{
+						if (arr[i][j - 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j - 1].topprob * .1;  // top
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+
+					}
+					else
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+					if (j < 7)
+					{
+						if (arr[i][j + 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j + 1].topprob * .1;  // bottom
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+						}
+
+
+					}
+					else
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+
+				}
+
+
+				
+
+
+					if (first.east == true)
+					{
+
+					}
+
+
+					if (first.south == true)
+					{
+
+					}
+
+
+				
+			}
+		}
+	}
 
 };
 
@@ -92,9 +470,17 @@ int main()
 	test.printmap();
 
 
+	sense first(false,false,false,false);
+	cout << endl << endl << endl;
+	test.firstsenseupdate(first);
+	
+	test.finalsenseupdate();
+	test.calculatebottom();
 
 
+	test.printmap();
 
+	
 
 
 }
