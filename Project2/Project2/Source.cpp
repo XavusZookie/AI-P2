cb5b61c4f022bb6a6c3a93ab7524cb5f937bdd77
XavusZookie <xavuszookie@gmail.com>
2022-11-06 21:36:33 -0500

    done
    made AI robot that moves and senses

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -5,10 +5,10 @@
 
 using namespace std;
 
-class squares
+class squares//for each section of the map
 {
 public:
-	//obstacle?
+	
 	bool west, north, east, south;
 	double topprob;
 	double temp;
@@ -19,11 +19,11 @@ public:
 		north = tnorth;
 		east = teast;
 		south = tsouth;
-		topprob = 1;
+		topprob = .0263;
 		temp = 0;
 	}
 
-	squares(double obstacle)
+	squares(double obstacle) // for the obstacle spaces
 	{
 		topprob = obstacle;
 		west = true;
@@ -36,10 +36,10 @@ public:
 
 };
 
-class sense
+class sense // holds the 4 boolean values for whats around the robot
 {
 public:
-	//obstacle
+	
 	bool west, north, east, south;
 	sense(bool twest, bool tnorth, bool teast, bool tsouth)
 	{
@@ -54,7 +54,7 @@ class map
 {
 public:
 	int width = 7, length = 6;
-	double currentbottom;
+	double currentbottom; // used in the normalization of the spaces as the bottom value(all the other values added toegether)
 	squares arr[6][7] = {
 	{squares(true, true, false, false),squares(false, true, false, true),squares(false, true, false, false),squares(false, true, false, false),squares(false, true, false, true),squares(false, true, false, false),squares(false, true, true, false)},
 	{squares(true, false, true, false),squares(0),squares(true, false, false, false),squares(false, false, true, false),squares(0),squares(true, false, false, false),squares(false, false, true, false)},
@@ -62,10 +62,10 @@ public:
 	{squares(true, false, true, false),squares(0),squares(true, false, false, false),squares(false, false, true, false),squares(0),squares(true, false, false, false),squares(false, false, true, false)},
 	{squares(true, false, false, false),squares(false, true, false, false),squares(false, false, false, false),squares(false, false, false, false),squares(false, true, false, false),squares(false, false, false, false),squares(false, false, true, false)},
 	{squares(true, false, false, true),squares(false, false, false, true),squares(false, false, false, true),squares(false, false, false, true),squares(false, false, false, true),squares(false, false, false, true),squares(false, false, true, true)},
-	};
+	};//the map filled with the spaces and their neighbor values
 
 	
-	void calculatebottom()
+	void calculatebottom() // adds the probability of each square so we can normalize them
 	{ 
 		currentbottom = 0;
 
@@ -82,25 +82,36 @@ public:
 	}
 	
 
-	void printmap()
+	void printmap()//prints the map... should i have put this comment? only time will tell, let me know in the feedback if i shouldnt have
 	{
+		
+			
+		
+		cout.precision(3); // rounds the values to 3 places
 		for (int i = 0; i < length; i++)
 		{
 			for (int j = 0; j < width; j++)
 			{
 				if (arr[i][j].topprob == 0)
 				{
-					cout << "#########" << "   ";
+					cout << "####" << "   ";//obstackles
+					
 				}
 				else
-				cout << arr[i][j].topprob/currentbottom << "   ";
+				{
+					cout << (arr[i][j].topprob) * 100 << "   ";//proababilities
+					
+				}
 			}
 			cout << endl;
 
 		}
+
+		cout << endl << endl << endl;
+
 	}
 
-	void firstsenseupdate(sense first)
+	void firstsenseupdate(sense first) //first half of the sensing calculates the first value for each square based off the sensing info and its neighbors values, will be normalized later
 	{
 		for (int i = 0; i < length; i++)
 		{
@@ -108,30 +119,30 @@ public:
 			{
 				if (arr[i][j].topprob != 0)
 				{
-					if (first.west == true)
+					if (first.west == true) // if the west space has an obstacle in the sensing
 					{
-						if (arr[i][j].west == true)
+						if (arr[i][j].west == true)//if this square has an obstacle to the west
 						{
 							arr[i][j].topprob *= .8;
 						}
-						else
+						else// if it doesnt 
 						{
 							arr[i][j].topprob *= .15;
 						}
 					}
-					else
+					else//if the sensing says the space to the west is open
 					{
-						if (arr[i][j].west == true)
+						if (arr[i][j].west == true)//if the west space actually has an obstacle
 						{
 							arr[i][j].topprob *= .2;
 						}
-						else
+						else//if the space is open to the west
 						{
 							arr[i][j].topprob *= .85;
 						}
 					}
 
-					if (first.north == true)
+					if (first.north == true)//same for north
 					{
 						if (arr[i][j].north == true)
 						{
@@ -154,7 +165,7 @@ public:
 						}
 					}
 
-					if (first.east == true)
+					if (first.east == true)//same for east
 					{
 						if (arr[i][j].east == true)
 						{
@@ -177,7 +188,7 @@ public:
 						}
 					}
 
-					if (first.south == true)
+					if (first.south == true)//you guessed it! for south
 					{
 						if (arr[i][j].south == true)
 						{
@@ -205,7 +216,7 @@ public:
 		}
 	}
 
-	void finalsenseupdate()
+	void finalsenseupdate()//uses the values calculated in firstsensorupdate and the current bottom to normalize all the values
 	{
 	
 
@@ -222,208 +233,203 @@ public:
 		}
 	}
 
-	void firstmovementupdate(int dir)
+	void firstmovementupdate(int dir)//updates the probabilities based off which way we tried to move
 	{
+
+		
+
+
 		for (int i = 0; i < length; i++)
 		{
 			for (int j = 0; j < width; j++)
 			{
 
-
-
-				if (dir == 1)
+					// 1 west 2 north 3 east 4 south im only gonna explain from west cause the others are all basically the same but from different directions
+				if (dir == 1) //from the west
 				{
 
-					if (i > 0)
+					if (i > 0) // if were not at thetop
 					{
-						if (arr[i - 1][j].topprob != 0)
+						if (arr[i - 1][j].topprob != 0)//if the space above us is open
 						{
-							arr[i][j].temp += arr[i - 1][j].topprob * .1;  // left
+
+							arr[i][j].temp += arr[i-1][j].topprob * .1; 
+
 						}
-						else
-						{
-							arr[i][j].temp += arr[i][j].topprob * .1;
-						}
-
-					}
-					else
-					{
-						arr[i][j].temp += arr[i][j].topprob * .1;
-					}
-
-					if (i < 6)
-					{
-
-						if (arr[i + 1][j].topprob != 0)
-						{
-							arr[i][j].temp += arr[i + 1][j].topprob * .8;  // right
-						}
-
-					}
-
-					if (j > 0)
-					{
-						if (arr[i][j - 1].topprob != 0)
-						{
-							arr[i][j].temp += arr[i][j - 1].topprob * .1;  // top
-						}
-						else
+						else//if theres an obstacle
 						{
 							arr[i][j].temp += arr[i][j].topprob * .1;
 						}
 
 
 					}
-					else
+					else// were at top of the map
 					{
 						arr[i][j].temp += arr[i][j].topprob * .1;
 					}
 
-					if (j < 7)
+					if (i < 5)// if were not at bottom of map
 					{
-						if (arr[i][j + 1].topprob != 0)
+
+						if (arr[i + 1][j].topprob != 0) // if the space below us is open
 						{
-							arr[i][j].temp += arr[i][j + 1].topprob * .1;  // bottom
+							arr[i][j].temp += arr[i + 1][j].topprob * .1;
+
 						}
-						else
+						else // the space is an obstacle
 						{
 							arr[i][j].temp += arr[i][j].topprob * .1;
+
+						}
+
+					}
+					else//were at bottom
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+
+					}
+
+					if (j > 0)//if were not at left wall
+					{
+						if (arr[i][j - 1].topprob != 0)//do nothing because we cant bounce off anything if its open
+						{
+							
+
+						}
+						else// theres an obstacle we can bounce off
+						{
+							arr[i][j].temp += arr[i][j].topprob * .8;
+
 						}
 
 
 					}
-					else
+					else//were at the left wall of the map
 					{
-						arr[i][j].temp += arr[i][j].topprob * .1;
+						arr[i][j].temp += arr[i][j].topprob * .8;
+
+
+					}
+
+					if (j < 6)// if were not at right wall
+					{
+						if (arr[i][j + 1].topprob != 0)// the space to the left of us is open 
+						{
+							arr[i][j].temp += arr[i][j + 1].topprob * .8;
+						}
+						else//noone can move into our square because the space is an obstacle and theres no chance we go backwards
+						{
+					
+
+						}
+
+
+					}
+					else//at right wall so nothing can move west to enter our space and we cant move backwards to bounce off the wall
+					{
+						
+
 					}
 
 
 				}
-					// 1 west 2 north 3 east 4 south
-
-				//done
-				if (dir == 1)
-					{
-
-						if (i > 0)
-						{
-							if (arr[i - 1][j].topprob != 0)
-							{
-								  // left
-							}
-							else
-							{
-								arr[i][j].temp += arr[i][j].topprob * .1;
-							}
-
-						}
-						else
-						{
-							arr[i][j].temp += arr[i][j].topprob * .1;
-						}
-
-						if (i < 6)
-						{
-
-							if (arr[i + 1][j].topprob != 0)
-							{
-								arr[i][j].temp += arr[i + 1][j].topprob * .8;  // right
-							}
-						
-						}
-
-						if (j > 0)
-						{
-							if (arr[i ][j-1].topprob != 0)
-							{
-								arr[i][j].temp += arr[i][j - 1].topprob * .1;  // top
-							}
-							else
-							{
-								arr[i][j].temp += arr[i][j].topprob * .1;
-							}
-
-							
-						}
-						else
-						{
-							arr[i][j].temp += arr[i][j].topprob * .1;
-						}
-
-						if (j < 7)
-						{
-							if (arr[i][j + 1].topprob != 0)
-							{
-								arr[i][j].temp += arr[i][j + 1].topprob * .1;  // bottom
-							}
-							else
-							{
-								arr[i][j].temp += arr[i][j].topprob * .1;
-							}
-
-
-						}
-						else
-						{
-							arr[i][j].temp += arr[i][j].topprob * .1;
-						}
-
-
-					}
 
 				if (dir == 2)
 				{
 
-					if (i > 0)
+					if (i > 0) // 0 = top
 					{
 						if (arr[i - 1][j].topprob != 0)
 						{
-							arr[i][j].temp += arr[i - 1][j].topprob * .1;  // left
+							 
+							
 						}
 						else
 						{
-							arr[i][j].temp += arr[i][j].topprob * .1;
+							arr[i][j].temp += arr[i][j].topprob * .8;
 						}
 
+
 					}
-					else
+					else// not top
 					{
-						arr[i][j].temp += arr[i][j].topprob * .1;
+						arr[i][j].temp += arr[i][j].topprob * .8;
 					}
 
-					if (i < 6)
+					if (i < 5)// not at bottom
 					{
 
 						if (arr[i + 1][j].topprob != 0)
 						{
-							arr[i][j].temp += arr[i + 1][j].topprob * .8;  // right
+							arr[i][j].temp += arr[i + 1][j].topprob * .8;  
+							
+						}
+						else
+						{
+							
 						}
 
 					}
+					else//at bottom
+					{
+						
+					}
 
-					if (j > 0)
+					if (j > 0)//not at left wall
 					{
 						if (arr[i][j - 1].topprob != 0)
 						{
-							arr[i][j].temp += arr[i][j - 1].topprob * .1;  // top
+							arr[i][j].temp += arr[i][j-1].topprob * .1;
+
 						}
 						else
 						{
 							arr[i][j].temp += arr[i][j].topprob * .1;
+
 						}
 
 
 					}
-					else
+					else//at left wall
 					{
 						arr[i][j].temp += arr[i][j].topprob * .1;
+						
+
 					}
 
-					if (j < 7)
+					if (j < 6)// not at right wall
 					{
 						if (arr[i][j + 1].topprob != 0)
 						{
-							arr[i][j].temp += arr[i][j + 1].topprob * .1;  // bottom
+							arr[i][j].temp += arr[i][j + 1].topprob * .1; 
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+
+						}
+
+
+					}
+					else//at right wall 
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+
+					}
+
+
+				}
+
+				if (dir == 3)
+				{
+
+					if (i > 0) // 0 = top
+					{
+						if (arr[i - 1][j].topprob != 0)
+						{
+
+							arr[i][j].temp += arr[i-1][j].topprob * .1;
+
 						}
 						else
 						{
@@ -432,55 +438,297 @@ public:
 
 
 					}
-					else
+					else// not top
 					{
 						arr[i][j].temp += arr[i][j].topprob * .1;
+					}
+
+					if (i < 5)// not at bottom
+					{
+
+						if (arr[i + 1][j].topprob != 0)
+						{
+							arr[i][j].temp += arr[i + 1][j].topprob * .1;
+
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+
+						}
+
+					}
+					else//at bottom
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+
+					}
+
+					if (j > 0)//not at left wall
+					{
+						if (arr[i][j - 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j - 1].topprob * .8;
+
+						}
+						else
+						{
+							
+
+						}
+
+
+					}
+					else//at left wall
+					{
+						
+
+
+					}
+
+					if (j < 6)// not at right wall
+					{
+						if (arr[i][j + 1].topprob != 0)
+						{
+							
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .8;
+
+						}
+
+
+					}
+					else//at right wall 
+					{
+						arr[i][j].temp += arr[i][j].topprob * .8;
+
+					}
+
+
+				}
+
+				if (dir == 4)
+				{
+
+					if (i > 0) // 0 = top
+					{
+						if (arr[i - 1][j].topprob != 0)
+						{
+
+							arr[i][j].temp += arr[i-1][j].topprob * .8;
+
+						}
+						else
+						{
+							
+						}
+
+
+					}
+					else// not top
+					{
+						
+					}
+
+					if (i < 5)// not at bottom
+					{
+
+						if (arr[i + 1][j].topprob != 0)
+						{
+							
+
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .8;
+
+						}
+
+					}
+					else//at bottom
+					{
+						arr[i][j].temp += arr[i][j].topprob * .8;
+
+					}
+
+					if (j > 0)//not at left wall
+					{
+						if (arr[i][j - 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j - 1].topprob * .1;
+
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+
+						}
+
+
+					}
+					else//at left wall
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+
+
+					}
+
+					if (j < 6)// not at right wall
+					{
+						if (arr[i][j + 1].topprob != 0)
+						{
+							arr[i][j].temp += arr[i][j + 1].topprob * .1;
+						}
+						else
+						{
+							arr[i][j].temp += arr[i][j].topprob * .1;
+
+						}
+
+
+					}
+					else//at right wall 
+					{
+						arr[i][j].temp += arr[i][j].topprob * .1;
+
 					}
 
 
 				}
 
 
-				
-
-
-					if (first.east == true)
-					{
-
-					}
-
-
-					if (first.south == true)
-					{
-
-					}
-
-
-				
 			}
 		}
 	}
 
+	void finalmovementupdate()//update the probability of each space from a temporary storage to the main probability and reset the temperary storage
+	{
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+				if (arr[i][j].topprob == 0)
+				{
+					
+				}
+				else
+				{
+					arr[i][j].topprob = arr[i][j].temp;
+					arr[i][j].temp = 0;
+				}
+
+			}
+			
+		}
+	}
 };
 
 int main()
 {
 	map test;
+	sense first(false, false, false, false);
+	sense second(true, false, false, false);
+	sense third(false, false, false, false);
+	sense fourth(false, true, false, true);
+	sense fifth(true, false, false, false);
+	// ^ these are self explanatory right?
+
 	test.calculatebottom();
+
+	cout << "Inital probabilities" << endl;
+
+	test.printmap();
+
+	test.firstsenseupdate(first);//for sensing this is the order because you get the values updated
+	test.calculatebottom();// then you get all the updated values added together 
+	test.finalsenseupdate();// then you use that to normalize them
+
+	
+	cout << "After sensing (0,0,0,0)" << endl;
+
+	test.printmap();
+	
+
+
+	test.firstmovementupdate(2);
+
+	test.finalmovementupdate();
+
+
+	cout << "After moving north" << endl;
+
 	test.printmap();
 
 
-	sense first(false,false,false,false);
-	cout << endl << endl << endl;
-	test.firstsenseupdate(first);
 	
+	test.firstsenseupdate(second);
+	test.calculatebottom();
+
 	test.finalsenseupdate();
-	test.calculatebottom();
-
+	cout << "After sensing (1,0,0,0)" << endl;
 
 	test.printmap();
 
 	
+
+	test.firstmovementupdate(2);
+
+	test.finalmovementupdate();
+	cout << "After moving north" << endl;
+
+	test.printmap();
+
+
+
+	test.firstsenseupdate(third);
+	test.calculatebottom();
+
+	test.finalsenseupdate();
+	cout << "After sensing (0,0,0,0)" << endl;
+
+	test.printmap();
+
+
+
+	test.firstmovementupdate(1);
+
+	test.finalmovementupdate();
+	cout << "After moving west" << endl;
+
+	test.printmap();
+
+
+
+	test.firstsenseupdate(fourth);
+	test.calculatebottom();
+
+	test.finalsenseupdate();
+	cout << "After sensing (0,1,0,1)" << endl;
+
+	test.printmap();
+
+
+
+	test.firstmovementupdate(1);
+
+	test.finalmovementupdate();
+	cout << "After moving west" << endl;
+
+	test.printmap();
+
+
+
+	test.firstsenseupdate(fifth);
+	test.calculatebottom();
+
+	test.finalsenseupdate();	
+	cout << "After sensing (1,0,0,0)" << endl;
+
+	test.printmap();
+
+	//hi
 
 
 }
